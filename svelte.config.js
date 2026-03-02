b11475c29cac9397fbed84964ae49729773f0846
Jitendra Singh Nimod <i.jitendra.singh0@gmail.com>
2026-03-02 17:21:53 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,8 @@
-// svelte.config.js
-
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = {
-	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter(),
-		alias: {
-			$lib: 'src/lib'
-		}
+		adapter: adapter()
 	}
 };
 
